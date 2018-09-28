80d3600c3f5e4f8af6c613997ff1315aada49820
walpolec <walpolec@tcd.ie>
2018-09-28 16:53:54 +0100

    Added lowestCommonAncestor function

--- CS3012 - Ass 1/src/LCA.java
+++ CS3012 - Ass 1/src/LCA.java
@@ -1,16 +1,32 @@
+class Node
+	 { 
+		 int key; 
+	     Node left, right; 
+	 
+	     public Node(int value)
+	     { 
+	    	 key = value; 
+	         left = right = null; 
+	     } 
+	 } 
 
 public class LCA
 {
-	
-	 class Node
+	 Node lowestCommonAncestor(Node node, int no1, int no2)  
 	 { 
-	        int key; 
-	        Node left, right; 
-	 
-	        public Node(int value)
-	        { 
-	            key = value; 
-	            left = right = null; 
-	        } 
+		 if (node == null) 
+		 {
+			 return null;
+		 }
+	     if (node.key > no1 && node.key > no2)
+	     {
+	            return lowestCommonAncestor(node.left, no1, no2); 
+	     }
+	     if (node.key < no1 && node.key < no2)
+	     {
+	            return lowestCommonAncestor(node.right, no1, no2); 
+	     }
+	     return node; 
 	 } 
 }
+
