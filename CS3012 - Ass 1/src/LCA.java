2eeb8d4a29719a0753b5fd42d0d4734ab5409a5d
walpolec <walpolec@tcd.ie>
2018-09-30 22:11:13 +0100

    Fixed for non existing nodes

--- CS3012 - Ass 1/src/LCA.java
+++ CS3012 - Ass 1/src/LCA.java
@@ -12,21 +12,47 @@ class Node
 
 public class LCA
 {
-	 static Node lowestCommonAncestor(Node node, int no1, int no2)  
-	 { 
-		 if (node == null) 
-		 {
-			 return null;
-		 }
-	     if (node.key > no1 && node.key > no2)
-	     {
-	            return lowestCommonAncestor(node.left, no1, no2); 
-	     }
-	     if (node.key < no1 && node.key < no2)
-	     {
-	            return lowestCommonAncestor(node.right, no1, no2); 
-	     }
-	     return node; 
-	 } 
+	static Node search(Node root, int value) 
+	{ 
+	    if (root==null || root.key==value) 
+	    {
+	        return root;
+	    }
+	    if (root.key > value)
+	    {
+	        return search(root.left, value);
+	    } 
+	    return search(root.right, value); 
+	} 
+	
+	static boolean present(Node root, int value)
+	{
+		if (search(root, value) == null)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	static Node lowestCommonAncestor(Node node, int no1, int no2)  
+	{ 
+		if (present(node, no1) & present(node, no2) == true)
+		{
+		if (node == null) 
+		{
+			return null;
+		}
+	    if (node.key > no1 && node.key > no2)
+	    {
+	        return lowestCommonAncestor(node.left, no1, no2); 
+	    }
+	    if (node.key < no1 && node.key < no2)
+	    {
+	        return lowestCommonAncestor(node.right, no1, no2); 
+	    }
+	    return node; 
+		}
+		else return null;
+	} 
 }
 
